6238b590804f0cea997002b5196b28c96988bbf1
silviav12 <silviav12@users.noreply.github.com>
2017-11-10 10:32:27 +0000

    Add files via upload

---
+++ LSTM_hmdb_topredic_tosubmit.py
@@ -0,0 +1,352 @@
+import numpy as np
+import tensorflow as tf
+import time
+import datetime, time
+import os
+import random
+import csv
+
+# IN this file, X refers to data and y to labels 
+
+# Initialize network variables, will be updated later in learnExternalData
+# and predictExternalData
+class init_vars:
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+    n_hidden = 32 
+    n_classes = 1 #Total classes: will be updated later
+    learning_rate = 0.03
+    lambda_loss_amount = 0.003 
+    its = 1000
+    batch_size = 350 
+    display_iter = 1000 
+    test_num = 72
+    valid_num = " "
+
+# Write variables to file
+def write_vars(values):
+    file = open("/homes/sv212/LSTM-Human-Activity-Recognition-master/plots/" + str(values.st) + "net.txt", "w")
+    file.write("n_hidden " + str(values.n_hidden) + "\n")
+    file.write("n_classes " + str(values.n_classes) + "\n")
+    file.write("learning_rate " + str(values.learning_rate) + "\n")
+    file.write("lambda_loss_amount " + str(values.lambda_loss_amount) + "\n")
+    file.write("training_iters " + str(values.its) + "\n")
+    file.write("batch size " + str(values.batch_size) + "\n")
+    file.write("lstm_cells " + str(1) + "\n")
+    file.write("test_num " + str(values.test_num) + "\n")
+    file.write("valid_num " + values.valid_num + "\n")
+    file.close() 
+
+# Function to define the strucure of the network
+def LSTM_RNN(_X, _weights, _biases, _n_hidden, n_input, n_steps):
+
+    # Reshape to prepare input to hidden activation 
+    # Input shape: (batch_size, n_steps, n_input)
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    _X = tf.reshape(_X, [-1, n_input]) 
+    
+    # Linear activation
+    _X = tf.matmul(_X, _weights['hidden']) + _biases['hidden']
+    
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(0, n_steps, _X) 
+
+    # Define network with three stacked LSTM cells with _n_hidden units
+    lstm_cell_1 = tf.nn.rnn_cell.BasicLSTMCell(_n_hidden, forget_bias=3)
+    lstm_cell_2 = tf.nn.rnn_cell.BasicLSTMCell(_n_hidden, forget_bias=3) 
+    lstm_cell_3 = tf.nn.rnn_cell.BasicLSTMCell(_n_hidden, forget_bias=3)
+    lstm_cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell_1, lstm_cell_2, lstm_cell_3])
+    
+    # Get LSTM cell output
+    outputs, states = tf.nn.rnn(lstm_cells, _X, dtype=tf.float32)
+
+    # Linear activation
+    return tf.matmul(outputs[-1], _weights['out']) + _biases['out']
+
+# Function to fetch a batch of data from "_train" of size "batch_size"
+def extract_batch_size(_train, step, batch_size, arr): 
+    shape = list(_train.shape)
+    shape[0] = batch_size
+    batch_s = np.empty(shape)
+    for i in range(batch_size):
+        index = arr[((step-1)*batch_size + i) % len(_train)]
+        batch_s[i] = _train[index] 
+
+    return batch_s
+
+# Function to convert single labels to matrix form
+def one_hot(y_, n_values):
+    y_ = y_.reshape(len(y_))
+    return np.eye(n_values)[np.array(y_, dtype=np.int32)]  
+
+
+# Function to calculate the cross entropy cost
+def calculate_cost(output, target):
+    # Compute cross entropy for each frame.
+    cross_entropy = -tf.reduce_sum(target*tf.log(tf.clip_by_value(output,1e-10,1.0)))
+    #cross_entropy = target * tf.log(output)
+    cross_entropy = -tf.reduce_sum(cross_entropy) 
+    mask = tf.sign(tf.reduce_max(tf.abs(target))) 
+    cross_entropy *= mask
+    # Average over actual sequence lengths.
+    cross_entropy = tf.reduce_sum(cross_entropy) 
+    cross_entropy /= tf.reduce_sum(mask) 
+    return tf.reduce_mean(cross_entropy)
+
+
+# Function to set up and train LSTM (in this function, test refers to validation data)
+def setup_and_train_LSTM(var_, X_train, y_train, X_test, y_test, test_num, valid_ids, fname):
+    tf.reset_default_graph()
+    prev_acc = 0.000
+    
+    # Initialize network variables
+    training_data_count = len(X_train)  
+    test_data_count = len(X_test)  
+    n_steps = len(X_train[0])  
+    n_input = len(X_train[0][0])  
+    n_hidden = var_.n_hidden
+    n_classes = var_.n_classes 
+    learning_rate = var_.learning_rate
+    lambda_loss_amount = var_.lambda_loss_amount 
+    training_iters = training_data_count * var_.its 
+    batch_size = var_.batch_size
+    display_iter = var_.display_iter  
+    
+    # Graph input/output
+    x = tf.placeholder(tf.float32, [None, n_steps, n_input])
+    y = tf.placeholder(tf.float32, [None, n_classes])
+
+    # Graph weights
+    weights = {
+        'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])), # Hidden layer weights
+        'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+    }
+    biases = {
+        'hidden': tf.Variable(tf.random_normal([n_hidden])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    pred = LSTM_RNN(x, weights, biases, n_hidden, n_input, n_steps)
+
+    # Set up loss, optimizer and evaluation with L2 regularization
+    l2 = lambda_loss_amount * sum(
+        tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables()
+    )
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y)) + l2 # Softmax loss
+    global_step = tf.Variable(0, trainable=False)
+    starter_learning_rate = var_.learning_rate
+    decayed_learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,100000, 0.96, staircase=True)
+    optimizer = tf.train.AdamOptimizer(learning_rate=decayed_learning_rate).minimize(cost, global_step=global_step)
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+ 
+	# Initialize variables to store loss and accuracy
+    test_losses = []
+    test_accuracies = []
+    test_step = []
+    train_losses = []
+    train_accuracies = []  
+
+    # Launch the graph
+    saver = tf.train.Saver(write_version=tf.train.SaverDef.V1)
+    sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    train_writer = tf.train.SummaryWriter("/homes/sv212/LSTM-Human-Activity-Recognition-master/tensorboard/", sess.graph)
+    
+    # Perform batch training 
+    step = 1
+    highest_acc = 0.00
+    n_examples = len(y_train)
+    steps_per_epoch =  int(n_examples/batch_size)
+    arr = np.arange(n_examples)
+    np.random.shuffle(arr)
+    while step * batch_size <= training_iters:
+        if (step % steps_per_epoch == 0):
+            arr = np.arange(n_examples)
+            np.random.shuffle(arr)
+            print("NEW EPOCH")
+        batch_xs = extract_batch_size(X_train, step, batch_size, arr)
+        batch_ys = one_hot(extract_batch_size(y_train, step, batch_size, arr), n_classes)
+
+        # Fit training using batch data
+        _, loss, acc = sess.run(
+            [optimizer, cost, accuracy],
+            feed_dict={
+                x: batch_xs, 
+                y: batch_ys
+            }
+        )
+        train_losses.append(loss)
+        train_accuracies.append(acc)
+		
+        acc_training = np.float32(acc)
+        
+        # Evaluate network only at some steps for faster training: 
+        if (step * batch_size % display_iter == 0) or (step == 1) or (step * batch_size > training_iters) or (step % steps_per_epoch == 0):
+            # Evaluation on the test set (no learning here)
+            loss, acc = sess.run(
+                [cost, accuracy], 
+                feed_dict={
+                    x: X_test,
+                    y: one_hot(y_test, n_classes)
+                }
+            )
+            test_losses.append(loss)
+            
+            if(np.float32(acc) > highest_acc):
+               highest_acc = np.float32(acc)
+               # Store session
+               saver.save(sess, "/vol/aesop/homes/sv212/thetis_models/models_" +  fname + "/model_" + fname + str(test_num) + "b" + str(batch_size) + "h" + str(n_hidden) + "v" + valid_ids + ".ckpt")
+               print("BEST ACC SO FAR: " + str(highest_acc))
+            print ("PERFORMANCE ON TEST SET: " + "Batch Loss = {}".format(loss) + ", Accuracy = {}".format(acc))
+            summary = tf.Summary(value=[tf.Summary.Value(tag="summary_tag", simple_value=np.float32(acc).item(acc)),])
+            train_writer.add_summary(summary)
+        step += 1
+
+    print ("Optimization Finished!")
+
+   
+	# Make predictions on test data
+    one_hot_predictions, accuracy, final_loss = sess.run(
+        [pred, accuracy, cost],
+        feed_dict={
+            x: X_test,
+            y: one_hot(y_test, n_classes)
+        }
+    )
+
+    test_losses.append(final_loss)
+    test_accuracies.append(accuracy)
+    test_step.append(step+1)
+    
+    print ("FINAL RESULT: " + "Batch Loss = {}".format(final_loss) + ", Accuracy = {}".format(accuracy))
+    print(test_accuracies)
+    print(test_step)
+	
+	
+# Function to make predictions on test data from pre-trained LSTM
+def predict(var_, X_train, X_test, y_test, model_name, model_num, fname):
+    tf.reset_default_graph()
+    prev_acc = 0.000
+    
+    # Initialize network variables
+    training_data_count = len(X_train)  
+    test_data_count = len(X_test)  
+    n_steps = len(X_train[0])  
+    n_input = len(X_train[0][0])  
+    n_hidden = var_.n_hidden
+    n_classes = var_.n_classes 
+    print(n_classes)
+    learning_rate = var_.learning_rate
+    lambda_loss_amount = var_.lambda_loss_amount 
+    training_iters = training_data_count * var_.its 
+    batch_size = var_.batch_size
+    display_iter = var_.display_iter  
+    
+    # Graph input/output
+    x = tf.placeholder(tf.float32, [None, n_steps, n_input])
+    y = tf.placeholder(tf.float32, [None, n_classes])
+
+    # Graph weights
+    weights = {
+        'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])), # Hidden layer weights
+        'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+    }
+    biases = {
+        'hidden': tf.Variable(tf.random_normal([n_hidden])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    pred = LSTM_RNN(x, weights, biases, n_hidden, n_input, n_steps)
+
+    
+    # Set up loss, optimizer and evaluation with L2 regularization
+    l2 = lambda_loss_amount * sum(
+        tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables()
+    )
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y)) + l2 
+    global_step = tf.Variable(0, trainable=False)
+    starter_learning_rate = var_.learning_rate
+    decayed_learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,100000, 0.96, staircase=True)
+    optimizer = tf.train.AdamOptimizer(learning_rate=decayed_learning_rate).minimize(cost, global_step=global_step)
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+     
+    saver = tf.train.Saver()
+    sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    
+    # Restore session of learned network from training data
+    saver.restore(sess, model_name)
+    
+    # Make predictions
+    pred, acc, loss = sess.run(
+        [pred, accuracy, cost],
+        feed_dict={
+            x: X_test,
+            y: one_hot(y_test, n_classes)
+        }
+    )
+    print(acc)
+    print(loss)
+    
+    # Directory to save predictions
+    csv_path = "/homes/sv212/trainTHETIS/predictions_" + fname + "/predictions" + str(model_num) + ".csv"
+    csv_path2 = "/homes/sv212/trainTHETIS/predictions_" + fname + "/proba" + str(model_num) + ".csv"
+    
+    myfile = open(csv_path, "w") 
+    wr = csv.writer(myfile)
+    myfile2 = open(csv_path2, "w") 
+    wr2 = csv.writer(myfile2)
+    for j in range(len(pred)): 
+        val_pred = np.argmax(pred[j]) 
+        dat = np.array([y_test[j], val_pred])
+        wr.writerow(dat)
+        dat2 = np.array([y_test[j]])
+        dat2 = np.append(dat2,pred[j])
+        wr2.writerow(dat2)
+        
+# Save file with hypreparameters and launch learning
+def learnExternalData(classesnum, X_train, y_train, X_valid, y_valid, valid_num, valid_ids, fname):
+	max_feats = 2048
+	max_time = 20	
+		
+	v = init_vars
+	v.n_hidden = 90
+	v.learning_rate = 0.001
+	v.lambda_loss_amount = 0.003
+	v.batch_size = 50
+	v.its = 350 
+	ts = time.time()
+	v.n_classes = classesnum
+	v.st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')	
+	v.test_num = valid_num
+	v.valid_num = valid_ids
+	write_vars(v)
+	# Start Learning (learned model will be save in learnExternalData)
+	setup_and_train_LSTM(v, X_train, y_train, X_valid, y_valid, valid_num, valid_ids, fname)
+
+# Select model and launch prediction
+def predictExternalData(classesnum, X_train, X_test, y_test, model_name, model_num, batch_s, hidden_s, lr, fname):
+	max_feats = 2048
+	max_time = 20			
+	v = init_vars
+	v.n_hidden = 90
+	v.learning_rate = 0.001
+	v.lambda_loss_amount = 0.003
+	v.batch_size = 50 
+	v.its = 350 
+	ts = time.time()
+	v.n_classes = classesnum
+	v.st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')	
+	# Return predictions
+	return predict(v,X_train, X_test,y_test, model_name, model_num,fname)
+		
+
+#if __name__ == '__main__':
+#	main()
+
+
+
---
+++ getFeatsHMDB_tosubmit.py
@@ -0,0 +1,98 @@
+# Generate csv files with inception features from video files using model available from:
+# https://github.com/tensorflow/models/blob/master/tutorials/image/imagenet/classify_image.py
+
+#classify_image from https://github.com/tensorflow/models/blob/master/tutorials/image/imagenet/classify_image.py
+import classify_image
+import cv2
+import numpy as np
+import subprocess as sp
+import glob
+import csv
+from skimage.feature import hog
+import os.path
+from skimage import data, color, exposure
+import sys
+
+# Find video dimensions to extract frames using ffmprobe
+def get_framedims(video):
+    print(video)
+    FFMPROBE_BIN = "ffprobe" 
+    command = [ FFMPROBE_BIN,
+            '-v', 'error',
+            '-show_entries', 'stream=width, height',
+            '-of', 'default=noprint_wrappers=1', video]
+    pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+    dims = pipe.stdout.read(20)
+    dstring = str(dims, 'utf-8')
+    A = dstring.split('width=')
+    B = A[1].split("\n")
+    height = int((B[1].split('='))[1])
+    width = int(B[0])
+    pipe.stdout.flush()
+    return height, width
+
+# Open video using ffmpeg
+def get_video(video):
+    FFMPEG_BIN = "ffmpeg" # on Linux ans Mac OS
+    command = [ FFMPEG_BIN,
+            '-i', video,
+            '-f', 'image2pipe',
+            '-pix_fmt', 'rgb24',
+            '-vcodec', 'rawvideo', '-']
+    pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+    return pipe
+
+# Given video dimensions, read video frames and return as array
+def read_frames(pipe, height, width):
+    raw_image = pipe.stdout.read(height*width*3)
+    # transform the byte read into a numpy array
+    image1 =  np.fromstring(raw_image, dtype='uint8')
+    if(image1.shape[0]!=height*width*3):
+        return False, image1
+    image1 = image1.reshape((height,width,3))
+    # throw away the data in the pipe's buffer.
+    pipe.stdout.flush()
+    return True, image1
+
+
+# Function to set up and train LSTM
+def main():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    fnames =  glob.glob("/vol/aesop/homes/sv212/hmdb_51/hmdb51_video/*")
+    dir_csv = "/vol/aesop/homes/sv212/hmdb_51/hmdb51_csv/"
+    # fnames = glob.glob("/export/vol/hsd/svdata/THETIS/VIDEO_RGB/*")
+	# dir_csv = "/export/vol/hsd/svdata/THETIS/THETIS_hog/"
+    for i in fnames[a:b]:
+        vnames = glob.glob(i + "/*.avi")
+        for v in vnames:
+            direc_ = v.split('/')[-2]
+            v1 = v.split('/')[-1]
+            my_file_name = v1.split('.avi')[0]+".csv"
+            if not os.path.exists(dir_csv + direc_ + "/"):
+                 os.makedirs(dir_csv + direc_ + "/")
+            csv_path = dir_csv + direc_ + "/" + my_file_name
+            if not(os.path.isfile(csv_path)):
+               print(v)
+               p = get_video(v)
+               h, w = get_framedims(v)
+               val = True
+               imList = []
+               while(val==True):
+				   # read frames
+                    val, im = read_frames(p, h, w)
+                    if(val==True):
+                        imList.append(im)
+               # Run inference from tensorflow code to get inception features
+               one = classify_image.run_inference_on_frame(imList)
+               myfile = open(csv_path, "w")
+               print(myfile)
+               # store as csv with same name as video and frame number 
+               # (video name contains label)
+               wr = csv.writer(myfile)
+               for j in one:    
+                    wr.writerow(j)
+
+if __name__ == '__main__':
+    main()
+
